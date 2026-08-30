845d7106f29e653c5230c0c7b16253c00a54e8c3
J Bond <jobomisc@icloud.com>
2026-08-30 20:45:43 +0100

    Initial Commit

---
+++ EmailClient/EmailClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmailClientApp.swift
+//  EmailClient
+//
+//  Created by Joseph Bond on 30/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct EmailClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
