5a941f853e30cf14b92cdde504262e70cb8f2822
J Bond <jobomisc@icloud.com>
2026-09-07 22:43:15 +0100

    File purpose added in comment

---
+++ Shared/Views/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  EmailClient
+//
+//  Created by Joseph Bond on 31/08/2026.
+//
+//This will be the view that includes a toolbar at the bottom and the other views above (sent/inbox etc)
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
