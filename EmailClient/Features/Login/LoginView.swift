790ea863e5f242dd4679ecf5ce7506fe4dd5f6ef
J Bond <jobomisc@icloud.com>
2026-09-07 22:47:55 +0100

    Add username and password fields with toggle to hide and show password. Add Login button anc calls to LoginViewModel for logic

--- EmailClient/Features/Login/LoginView.swift
+++ EmailClient/Features/Login/LoginView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @StateObject var viewModel: LoginViewModel   // owns the lifetime
+    @State var isPasswordVisible: Bool //private, default value false
+    
+    
+    @Binding var isLoggedIn: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+            TextField("Username", text: $viewModel.username)
+                .autocorrectionDisabled(true)
+            #if os(iOS)
+                .textInputAutocapitalization(.never)
+            #endif
+            
+            //Username and password fields
+            HStack{
+                //Logic for showing password
+                if isPasswordVisible {
+                    //Binding of $varName allows the variable value to be updated
+                    TextField("Password", text: $viewModel.password).autocorrectionDisabled(true)
+                }else{
+                    SecureField("Password", text: $viewModel.password).autocorrectionDisabled(true)
+                }
+                
+                Button(action: {
+                    isPasswordVisible.toggle()
+                }){
+                    Image(systemName: isPasswordVisible ? "eye" : "eye.slash")
+                }
+            }
+            
+            //Login button
+            Button(action: {
+                //update var with entered credentials
+                isLoggedIn = viewModel.checkDetails()
+            }){
+                Text("Login")
+            }
+        }.padding()
     }
 }
 
+
 #Preview {
-    LoginView()
+    //set isLoggedIn to false constantly for testing,
+    LoginView(viewModel: LoginViewModel(password: "dsa", username: "asd"), isPasswordVisible: true, isLoggedIn: .constant(false))
 }
